a3ebe1e42b3c35ae5aca7f29c229122bb8151d01
Raphael Monneron <rglmonneron@gmail.com>
2022-01-19 17:45:22 +0100

    basic_project_structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+    <Container className="p-5 mb-4 bg-light">
+      <Button variant="primary">Blue</Button>
+    </Container>
+  );
+}
+
+export default App;
