f67138b234fa8ba1346cf19bcda6ff0aca1484a6
Raphael Monneron <rglmonneron@gmail.com>
2022-02-04 14:55:29 +0100

    Mark III

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,47 +1,64 @@
-import { Carousel, Container } from "react-bootstrap";
+import { Card, Button, CardGroup } from "react-bootstrap";
 
 function Projects() {
   return (
-    <div>
-      <Container>
-        <Carousel variant="dark">
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="tatooine.jpg"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="tatooine.jpg"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="tatooine.jpg"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h5>Third slide label</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </Container>
+    <div className="bg-primary">
+      <CardGroup>
+        <Card style={{ width: "18rem" }} className="p-3 border-0">
+          <Card.Img variant="top" src="hotelchambord.jpg" />
+          <Card.Body>
+            <Card.Title className="text-center">Projet Statique</Card.Title>
+            <Card.Text>
+              Premier projet d'un site d'hotel de luxe. (HTML, CSS, JAVASCRIPT)
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer className="mx-auto">
+            <Button variant="warning">
+              <a
+                target="_blank"
+                href="https://raphael-monneron.fr/hotel_chambord/"
+              >
+                voir
+              </a>
+            </Button>
+          </Card.Footer>
+        </Card>
+        <Card style={{ width: "18rem" }} className="p-3 border-0">
+          <Card.Img variant="top" src="swap.jpg" />
+          <Card.Body>
+            <Card.Title className="text-center">Projet Dynamique</Card.Title>
+            <Card.Text>
+              Second projet d'un site dynamique type le bon coin (HTML, CSS,
+              JAVASCRIPT, PHP)
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer className="mx-auto">
+            <Button variant="warning">
+              <a target="_blank" href="https://swap-monneron.lescigales.org/">
+                voir
+              </a>
+            </Button>
+          </Card.Footer>
+        </Card>
+        <Card style={{ width: "18rem" }} className="p-3 border-0">
+          <Card.Img variant="top" src="mt2021.jpg" />
+          <Card.Body>
+            <Card.Title className="text-center">
+              Projet de Soutenance
+            </Card.Title>
+            <Card.Text>
+              Dernier projet sous CMS (wordpress, woocommerce)
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer className="mx-auto">
+            <Button variant="warning">
+              <a target="_blank" href="https://mt2021.fr/">
+                voir
+              </a>
+            </Button>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
     </div>
   );
 }
